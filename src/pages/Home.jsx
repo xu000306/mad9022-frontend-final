fb404c155e58bd5aa3f917e505f8ebc41b177743
chab0114 <chab0114@algonquinlive.com>
2025-03-27 12:25:04 -0400

    Set up basic Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div className="home">
+            <h2>Welcome to HollyCrapr</h2>
+            <p>Find items people are looking to get rid of!</p>
+        </div>
+    )
+}
+
+export default Home
