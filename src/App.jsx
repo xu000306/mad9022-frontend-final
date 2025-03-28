68412abe5edf844bbd3fa6524bae4fe6523d7981
chab0114 <chab0114@algonquinlive.com>
2025-03-28 15:04:37 -0400

    Improved header alignment with container wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Header from './components/Header'
-import styles from './App.module.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Header from './components/Header';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className={styles.app}>
+    <div>
       <Header />
       <main className={styles.main}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path='/about' element={<About/>}/>
+          <Route path="/about" element={<About />} />
         </Routes>
       </main>
     </div>
-  )
+  );
 }
 
 export default App;
