5c717a7efbd4e33a32583dc1dfbeff2e9787a531
chab0114 <chab0114@algonquinlive.com>
2025-03-31 00:43:31 -0400

    Add search result page - Crap with mock data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Crap from './pages/Crap'
 import Header from './components/Header';
 import styles from './App.module.css';
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/crap" element={<Crap />} />
         </Routes>
       </main>
     </div>
