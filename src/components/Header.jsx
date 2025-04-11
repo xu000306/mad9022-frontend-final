5067a979d1a809ef241aa877f8fc89f2bf16d8e6
xu (Tom) weikai <xu000306@algonquinlive.com>
2025-04-10 20:48:41 -0400

    log in

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
-
+import logo from "../assets/holycrap.png";
 function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
         <div className={styles.logo}>
-          <h1>HolyCrap</h1>
+          <img className={styles.logoImage} src={logo} alt="logo" />{" "}
+          <h1 className={styles.logoName}>HolyCrap</h1>
         </div>
         <nav className={styles.nav}>
           <Link className={styles.link} to="/">
